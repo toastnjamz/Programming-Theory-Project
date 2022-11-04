103f1a02b7f520b9b8f004630af8919b020be67f
Verity Davis <veritydavis@gmail.com>
2022-11-03 22:46:25 -0700

    Initial commit

---
+++ Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+
+{
+    public float rotationSpeed = 2000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
