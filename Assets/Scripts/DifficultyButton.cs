4206666965b856f61db02bffa775e67a86518851
Verity Davis <veritydavis@gmail.com>
2022-11-05 20:55:14 -0700

    Adding Functionality to Menu and Aircraft base class

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -7,14 +7,14 @@ public class DifficultyButton : MonoBehaviour
 {
     public int difficulty;
     private Button button;
-    //private GameManager gameManager;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(SetDifficulty);
-        //gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -26,6 +26,10 @@ public class DifficultyButton : MonoBehaviour
     private void SetDifficulty()
     {
         Debug.Log(gameObject.name + "was clicked");
-        //gameManager.StartGame(difficulty);
+        gameManager.StartGame(difficulty);
+
+        // if easy, select plane prefab and have game manager instantiate it and assign it to the player
+        // if medium, select prop plane 
+        // if hard, select jet
     } 
 }
